608acf3b4998778d2839f48ca68f2f76890a67d3
huangjinlai <huangjinlaijlu@163.com>
2025-04-20 12:06:35 +0800

    [alter]修改启动配置

--- avatar-business/src/main/java/com/hikvision/AvatarBusinessApplication.java
+++ avatar-business/src/main/java/com/hikvision/AvatarBusinessApplication.java
@@ -3,6 +3,7 @@ package com.hikvision;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 
 /**
  * @Description 主启动类
@@ -10,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2025/4/19 23:04
  * @Version 1.0
  **/
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class AvatarBusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(AvatarBusinessApplication.class, args);
