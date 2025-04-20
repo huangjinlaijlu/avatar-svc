f3074007b3ad48e5f12b3e42a4938ac746a3943a
huangjinlai <huangjinlaijlu@163.com>
2025-04-20 11:23:13 +0800

    [add]引入启动配置

---
+++ avatar-business/src/main/java/com/hikvision/AvatarBusinessApplication.java
@@ -0,0 +1,18 @@
+package com.hikvision;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 主启动类
+ * @Author Jinlai
+ * @Date 2025/4/19 23:04
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class AvatarBusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AvatarBusinessApplication.class, args);
+    }
+}
---
+++ avatar-business/src/main/java/com/hikvision/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.hikvision.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 测试类
+ * @Author Jinlai
+ * @Date 2025/4/20 11:15
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/test/")
+public class TestController {
+
+    @Value("${user.age}")
+    private String username;
+
+    @GetMapping("/inFile")
+    public String inFile() {
+        System.out.println(username);
+        return username;
+    }
+}
